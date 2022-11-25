51ed8929940b769a7fd845c0777543f7806ddb2f
pautowa <iulchi.pautowa@yandex.ru>
2022-11-25 23:18:04 +0300

    Added AdminPage and updated Header

---
+++ frontend/src/components/ClientsTrainersTable.js
@@ -0,0 +1,13 @@
+export default function ClientsTrainersTable(props){
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>ФИО</th>
+                    <th>Номер телефона</th>
+                    <th>Почта</th>
+                </tr>
+            </thead>
+        </table>
+    );
+};
