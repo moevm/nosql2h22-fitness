40294e96891876eef4f18db6543958a5392a832d
Roslova Larisa <Larisa1220@mail.ru>
2023-01-10 21:03:48 +0300

    lk timetable filter

--- backend/routes/users_route.js
+++ backend/routes/users_route.js
@@ -3,7 +3,8 @@ module.exports = function(app, db) {
     
     const clients_collection = db.collection('clients');
     const trainer_collection = db.collection('trainer');
-    const users_collection = db.collection('users')
+    const users_collection = db.collection('users');
+    const timetable_collection = db.collection('timetable');
 
     // Регистрация нового пользователя
     app.post('/registration', (req, res) => {
@@ -93,5 +94,87 @@ module.exports = function(app, db) {
         }
         getAllDocuments();
     })
+
+    app.post('/lk_timetable/filter', (req, res) => {
+        const fio = req.body.fio;
+        const date = req.body.date;
+        const time = req.body.time;
+    
+        let parametres = 0;
+        let timeOne;
+        let timeTwo;
+        let flag = 0;
+
+        if(time != " "){
+            let timearr = time.split(' ');
+            // console.log('timearr: '+ timearr);
+            if(timearr[1] == ''){
+                // console.log("я тут в сплите 1");
+                flag = 1;
+                timeOne = Date.parse(`${date}T${timearr[0]}:00Z`);
+                // timeTwo = Date.parse(`${date}T${timearr[0]}:00Z`);
+            }
+            if(timearr[0] == ''){
+                // console.log("я тут в сплите 2");
+                flag = 2;
+                // timeOne = Date.parse(`${date}T${timearr[1]}:00Z`);
+                timeTwo = Date.parse(`${date}T${timearr[1]}:00Z`);
+            }
+            if(timearr[0] != '' && timearr[1] != ''){
+                // console.log("я тут в сплите 3");
+                flag = 3;
+                timeOne = Date.parse(`${date}T${timearr[0]}:00Z`);
+                timeTwo = Date.parse(`${date}T${timearr[1]}:00Z`);
+            }
+            parametres = 1;
+        }
+
+        if(parametres == 0){
+            console.log("filter lk_timetable only date");  
+            filterOnlyDate();
+        }
+        if(parametres == 1){
+            console.log("filter lk_timetable with time");
+            (flag < 3) ? filterOneT() : filterTime();
+        }
+
+        async function filterOnlyDate() {
+            const tmp = await timetable_collection.find({$and:[{date: date}, {$or: [{listOfEnrolled: fio}, {trainer: fio}]}]}).toArray();
+            parseTime(tmp);
+            res.send(tmp);
+        }
+
+        async function filterTime() {
+            const tmp = await timetable_collection.find({$and: [{date: date}, {$or: [{listOfEnrolled: fio}, {trainer: fio}]}, {time: {$gte : timeOne, $lte: timeTwo}}]}).sort({"time": 1}).toArray();
+            parseTime(tmp);                                         
+            res.send(tmp);
+        }
+        async function filterOneT() {
+            if(flag == 2){
+                const tmp = await timetable_collection.find({$and:[{date: date},{$or: [{listOfEnrolled: fio}, {trainer: fio}]}, {time: {$lte: timeTwo}}]}).sort({"time": 1}).toArray(); 
+                parseTime(tmp);                                         
+                res.send(tmp);
+            }
+            if(flag == 1){
+                const tmp = await timetable_collection.find({$and:[{date: date},{$or: [{listOfEnrolled: fio}, {trainer: fio}]}, {time: {$gte : timeOne}}]}).sort({"time": 1}).toArray(); 
+                parseTime(tmp);                                         
+                res.send(tmp);
+            }
+            if(flag == 0){
+                const tmp = await timetable_collection.find({$and:[{date: date},{$or: [{listOfEnrolled: fio}, {trainer: fio}]}]}).toArray(); 
+                parseTime(tmp);                                         
+                res.send(tmp);
+            }
+            
+        }
+
+        function parseTime(arr){
+            for(let i = 0; i<arr.length; i++){
+               let tmp = new Date(arr[i].time).toISOString().substring(11, 16);
+               arr[i].time = tmp;
+            }
+        }
+
+    });
 
 }
