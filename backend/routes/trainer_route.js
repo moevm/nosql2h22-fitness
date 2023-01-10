692f6c610dedabec72e8c03ede2e3c4862276c71
Roslova Larisa <Larisa1220@mail.ru>
2023-01-10 18:34:07 +0300

    filters to trainers page

--- backend/routes/trainer_route.js
+++ backend/routes/trainer_route.js
@@ -1,20 +1,6 @@
 var ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, collection) {
 
-    // Фильтрация по имени
-    app.get('/trainers/filter/:fio', (req, res) => {
-        const fio = req.params.fio;
-        async function filter() {
-            try {
-            const tmp = await collection.find({FIO: fio}).toArray();
-                res.send(tmp)
-            }catch(err) {
-                console.log(err);
-            }
-        }
-        filter()
-    });
-    
     // Получение документа по имени
     app.get('/trainers/:fio', (req, res) => {
         const fio = req.params.fio;
@@ -42,77 +28,60 @@ module.exports = function(app, collection) {
         getAllDocuments();
     });
 
+    // Фильтрация по всем полям
+    // Вне зависисимости от того по каким парметрам ищем
+    // Если ищем по одному параметру - второй передается пустой строкой
     app.post('/trainers/filter', (req, res) => {
-        const fio = req.body.fio;
-        const tel = req.body.tel;
-        const email = req.body.email;
-        
-        let arr = [fio, tel, email];
+        const programm = req.body.programm;
+        const trainer = req.body.trainer;
+    
         let parametres = 0;
-        let fio_reg;
-        let tel_reg;
-        let email_reg;
+        let programm_reg;
+        let trainer_reg;
 
-        if(fio!=''){
-            fio_reg = new RegExp(`${fio}`, 'i');
+        if(programm!=''){
+            programm_reg = new RegExp(`${programm}`, 'i');
+            parametres++;
         }
-        if(tel!=''){
-            tel_reg = new RegExp(`${tel}`, 'i');
-        }
-        if(email!=''){
-            email_reg = new RegExp(`${email}`, 'i');
+        if(trainer!=''){
+            trainer_reg = new RegExp(`${trainer}`, 'i');
+            parametres++;
         }
         
-        for(let i =0; i<arr.length; i++){
-            if(arr[i] != ''){
-                parametres++;
-            }
+        if(parametres == 0){
+            console.log("send all trainers");
+            getAllDocuments();
         }
-
         if(parametres == 2){
-            console.log("filter 2 param");
             filterOnlyTwo();
-        }
-        if(parametres == 1){
-            console.log("filter 1 param");
-            filterOnlyOne();
+            console.log("filter trainers 2 param");
             
         }
-        if(parametres == 3){
-            console.log("filter 3 param");
-            filterAll();
-        }
-        if(parametres == 0){
-            console.log("filter 0 param");
-            getAllDocuments();
+        if(parametres == 1){
+            console.log("filter trainer 1 param");
+            filterOnlyOne();
         }
 
         async function filterOnlyTwo() {
-            // console.log("я тут, флаг 3");
-            const tmp = await collection.find({$or:[{$and:[{FIO: fio_reg},{telephone: tel_reg}]},
-                                                            {$and:[{FIO: fio_reg},{email: email_reg}]},
-                                                            {$and:[{telephone: tel_reg},{email: email_reg}]}
-                                                            ]}).toArray();    
+            const tmp = await collection.find({$and:[{programm: programm_reg},{FIO: trainer_reg}]}).toArray();    
             res.send(tmp);
         }
-
-        async function filterOnlyOne() {
-            const tmp = await collection.find({$or:[{FIO: fio_reg},{telephone: tel_reg},{email: email_reg}]}).toArray(); 
-            res.send(tmp);
-        }
-
-        async function filterAll() {
-            const tmp = await collection.find({FIO: fio_reg, telephone: tel_reg, email: email_reg}).toArray();                                          
-            res.send(tmp);
-        }
+        
         async function getAllDocuments() {
             try {
                 const tmp = await collection.find().toArray();
                 res.send(tmp)
+                 
             }catch(err) {
                 console.log(err);
             }
         }
+
+        async function filterOnlyOne() {
+            const tmp = await collection.find({$or:[{programm: programm_reg},{FIO: trainer_reg}]}).toArray();                                         
+            res.send(tmp);
+        }
+
     });
 
 }
