2df7b7e38fdb6378df8edafc81251c7a97b6744e
pautowa <iulchi.pautowa@yandex.ru>
2023-01-11 15:00:08 +0300

    updated timetable in lk and added filtering by time in lk of trainer

--- frontend/src/components/ClientPage.js
+++ frontend/src/components/ClientPage.js
@@ -1,40 +1,63 @@
 import '../css/ClientPage.css';
 
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 export default function ClientPage(){
     const [date, setDate] = useState(new Date());
     const [data, setData] = useState([]);
+    const FIO = JSON.parse(sessionStorage.getItem('autoriz')).FIO;
 
-    // useEffect(()=>{
-    //     axios
-    //         .post('/timetable/filter', {date: date.toISOString().slice(0,10)})
-    //         .then(res => {
-    //             console.log(res.data);
-    //             setData(res.data);
-    //         })
-    //         .catch(err => {
-    //             console.log('err in data', err);
-    //         });
-    // },[]);
+    useEffect(()=>{
+        axios
+            .post('/lk_timetable/filter', {fio: FIO, date: date.toISOString().slice(0,10), time:' '})
+            .then(res => {
+                // console.log(res.data);
+                setData(res.data);
+            })
+            .catch(err => {
+                console.log('err in data', err);
+            });
+    },[]);
 
     const handleChangeDate = e => {
-        // console.log(e);
         const offset = e.getTimezoneOffset();
         const valDate = new Date(e.getTime() - (offset*60*1000));
         setDate(valDate);
-        // console.log(valDate.toISOString().split('T')[0])
-        // axios
-        //     .post('/timetable/filter', {date: valDate.toISOString().slice(0,10)})
-        //     .then(res => {
-        //         // console.log(res);
-        //         setData(res.data);
-        //     })
-        //     .catch(err => {
-        //         console.log('err in data', err);
-        //     });
+        axios
+            .post('/lk_timetable/filter', {fio: FIO, date: valDate.toISOString().slice(0,10), time:' '})
+            .then(res => {
+                // console.log(res.data);
+                setData(res.data);
+            })
+            .catch(err => {
+                console.log('err in data', err);
+            });
+    };
+
+    const parseData = () => {
+        console.log(data.length);
+        if(!data.length){
+            return(
+                <div className='event-content'>
+                    <p>Тренировок нет.</p>
+                </div>
+            );
+        }
+        else{
+            return(
+                data.map(item=>{
+                    return(
+                        <div className='event-content' key={item.time}>
+                            <p>{item.time}</p>
+                            <p>{item.trainer}</p>
+                        </div>
+                    )
+                })
+            );
+        }
     };
 
     return(
@@ -44,7 +67,7 @@ export default function ClientPage(){
             </div>
             <div className='events-container'>
                 <div className='selected_date'>{date.toLocaleString('ru', {month: 'long', day:'numeric'})}</div>
-                <div className='event-content'>событие</div>
+                {parseData()}
             </div>
         </div>
     );
